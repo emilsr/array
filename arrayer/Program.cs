7d3ae6849e0234b8c9b2e3f5438cd35ee6a65e85
Emil Stener <stener99@hotmail.se>
2016-10-21 15:10:12 +0200

    asdasd

--- arrayer/Program.cs
+++ arrayer/Program.cs
@@ -10,7 +10,59 @@ namespace arrayer
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //övning1
+            /*
+            Console.WriteLine("mata in 10 olika namn");
+            string[] namn = { Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine() , Console.ReadLine() , Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine() };
+            for (int i = 0; i < namn.Length; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+            Console.ReadKey();
+            
+            //övning2
+
+            Console.WriteLine("mata in 10 heltal");
+            string[] tal = new string[10];
+            for (int i = 9; i >= 0; i--)
+            {
+                tal[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < tal.Length; i++)
+            {
+                Console.WriteLine(tal[i]);
+            }
+            Console.ReadKey();
+            
+            //övning3
+            int[] numbArray = { 12, 5, 6, 1, 2, 3, -2, -7, 0 };
+            int tal;
+            for (int i = 0; i < numbArray.Length; i++)
+            {
+                for (int x = 1; x < numbArray.Length-1; x++)
+                {
+                    if (numbArray[x] < numbArray[x+1])
+                    {
+
+                        tal = numbArray[x+1];
+                        numbArray[x + 1] = numbArray[x];
+
+                        numbArray[x] = tal;
+
+                    }
+                }
+            }
+                for (int i = 0; i < numbArray.Length; i++)
+                {
+                    Console.WriteLine(numbArray[i]+"");
+                }
+                
+            
+            Console.ReadKey();
+            */ 
+
+            //övning4
+
         }
     }
 }
